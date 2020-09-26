1a36df2d60bf2b26cebe6ba47191e44dd941a67b
Nasim Mahmud <nasim.mahmud.1996@gmail.com>
2020-09-26 22:45:01 +0600

    Update urls.py

--- personal/urls.py
+++ personal/urls.py
@@ -19,6 +19,7 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve as mediaserve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +30,13 @@ urlpatterns = [
     
 ]
 
+urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                     mediaserve, {'document_root': settings.MEDIA_ROOT}))
+
 urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+ 
+  
